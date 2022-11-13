8b640adefe8239556b88402577fa6d539178b39e
LuluahAld <90204390+LuluahAld@users.noreply.github.com>
2022-11-13 21:22:21 +0300

    Update navbar.php

--- bloodrop 2/navbar.php
+++ bloodrop 2/navbar.php
@@ -21,7 +21,7 @@ session_start();
     </style>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../CSS/style.css" /> 
-    <link rel="stylesheet" href="../CSS/style2.css" />
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.1.1/normalize.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     
