02b7f38c3788a089d5cfed77447de98c3eeb0d08
LuluahAld <90204390+LuluahAld@users.noreply.github.com>
2022-11-14 06:52:46 +0300

    Update navbar.php

--- bloodrop 2/navbar.php
+++ bloodrop 2/navbar.php
@@ -43,7 +43,7 @@ session_start();
             </a></li>
     
             <?php if (!isset($_SESSION['loggedin'])): ?>
-        <li class="dropdown" style= "float:right;>
+        <li class="dropdown" class = "right">
             <a href="javascript:void(0)" class="dropbtn">
                 <font size="+1.75"><b>User</b></font>
             </a>
